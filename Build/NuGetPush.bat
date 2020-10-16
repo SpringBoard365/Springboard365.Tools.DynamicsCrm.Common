8e8d5995b9d736b8b4e0458736b147888db3f9f1
David Small <dave@dsmall.co.uk>
2020-10-16 22:10:09 +0100

    chore: refactor folder structure

--- Build/NuGetPush.bat
+++ Build/NuGetPush.bat
@@ -1,6 +1,6 @@
 SET packageVersion=2.0.0-alpha02
 
-NuGet.exe pack ../Springboard365.Tools.DynamicsCrm.Common.csproj -Build -symbols -Version %packageVersion%
+NuGet.exe pack ../DynamicsCrm.Common.csproj -Build -symbols -Version %packageVersion%
 
 NuGet.exe push Springboard365.Tools.DynamicsCrm.Common.%packageVersion%.nupkg -Source "https://api.nuget.org/v3/index.json"
 
