22bca5e76373d06a9e58562b15775e93d807eea0
David Small <dave@dsmall.co.uk>
2020-10-21 22:11:43 +0100

    feat: Fix build script and update CommandLine.Core to 2.0.0.

--- Build/NuGetPush.bat
+++ Build/NuGetPush.bat
@@ -1,19 +1,21 @@
 @echo off
-SET packageVersion=2.0.1-beta02
+SET packageVersion=2.0.1-beta03
 
 SET configuration=Release
-SET id="Springboard365.Tools.DynamicsCrm.Common";
-SET author="Springboard 365 Ltd";
-SET repo="https://github.com/SpringBoard365/Springboard365.Tools.DynamicsCrm.Common";
-SET description="The common project to create command line tools for Dynamics Crm.";
-SET tags="CommandLine Springboard365ToolHelper PowerPlatformToolHelper Dynamics365ToolHelper DynamicsCrmToolHelper";
+SET id="Springboard365.Tools.DynamicsCrm.Common"
+SET author="Springboard 365 Ltd"
+SET repo="https://github.com/SpringBoard365/Springboard365.Tools.DynamicsCrm.Common"
+SET description="The common project to create command line tools for Dynamics Crm."
+SET tags="CommandLine Springboard365ToolHelper PowerPlatformToolHelper Dynamics365ToolHelper DynamicsCrmToolHelper"
 
-dotnet build ../src/DynamicsCrm.Common.csproj -c  %configuration% -p:Version=%packageVersion% -f net462 --nologo
+dotnet restore ../%id%.sln
+
+dotnet build ../%id%.sln -c  %configuration% -p:Version=%packageVersion% -f net462 --nologo
 
 pause
 
-NuGet.exe pack ../src/DynamicsCrm.Common.nuspec -Build -symbols -Version %packageVersion% -Properties Configuration=%configuration%;id=%id%;author=%author%;repo=%repo%;description=%description%;tags=%tags%;
+NuGet pack ../src/DynamicsCrm.Common.nuspec -Build -symbols -Version %packageVersion% -Properties Configuration=%configuration%;id=%id%;author=%author%;repo=%repo%;description=%description%;tags=%tags%;
 
-NuGet.exe push Springboard365.Tools.DynamicsCrm.Common.%packageVersion%.nupkg -Source "https://api.nuget.org/v3/index.json"
+NuGet push %id%.%packageVersion%.nupkg -Source "https://api.nuget.org/v3/index.json"
 
 pause
